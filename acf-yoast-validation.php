ba4d35591cadb3be53bd48d910b248ca6fd29fe1
Alex Leach <a.leach@biopharmaspec.com>
2016-11-11 12:51:06 +0000

    Minify everything

--- acf-yoast-validation.php
+++ acf-yoast-validation.php
@@ -12,17 +12,19 @@ Copyright: DACHCOM.DIGITAL, Stefan Hagspiel
 
 Class AcfYoastSeoValidator {
 
-    public function __construct() {
-        add_action( 'admin_enqueue_scripts',  array($this, 'bind_js'));
-    }
+  public function __construct() {
+    add_action( 'admin_enqueue_scripts',  array($this, 'bind_js'));
+  }
 
-    public static function bind_js() {
-        if (defined('WPSEO_VERSION')) {
-            wp_enqueue_script('acf_yoast_seo_validator',
-                plugin_dir_url( __FILE__ ) . 'acf_yoast.js',
-                array('acf-input'), false, true);
-        }
+  public static function bind_js() {
+    if (defined('WPSEO_VERSION')) {
+      $min = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '' : '.min';
+
+      wp_enqueue_script('acf_yoast_seo_validator',
+        plugin_dir_url( __FILE__ ) . "acf_yoast{$min}.js",
+        array('acf-input'), false, true);
     }
+  }
 }
 
 add_action( 'admin_init', 'acf_yoast_init' );
